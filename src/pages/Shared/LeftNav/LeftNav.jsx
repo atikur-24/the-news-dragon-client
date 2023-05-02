bd002e99f37411d3ec81a001eeea3038a1cd3861
Atikur <atikurrahman7223@gmail.com>
2023-05-02 13:14:02 +0600

    added right nav

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
-    return (
-        <div>
-            <p>this is left nav</p>
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div> 
+      <div>
+        <h5 className="fw-semibold">All Caterogy</h5>
+        <div className="mt-3 ps-3">
+            {
+                categories.map( category => <p key={category.id}>
+                    <Link className='text-secondary text-decoration-none font-500' to={`/category/${category.id}`}>{category.name}</Link>
+                </p>)
+            }
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
